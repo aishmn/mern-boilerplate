a3b2d94511e28a2112502cf4a4abbb718bf0b7c4
aishmn <manish.sinuwari@gmail.com>
2020-05-27 15:44:44 +0545

    updated for deployment

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import store from "./../redux/store";
 import { LOGOUT } from "../redux/action/types";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
