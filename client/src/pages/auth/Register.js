328e177f770634eab6802ee8c61c4f439e8e5506
aishmn <manish.sinuwari@gmail.com>
2020-05-27 15:48:33 +0545

    updated for deployment

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -28,7 +28,7 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
   };
 
   if (isAuthenticated) {
-    return <Redirect to="/dashboard" />;
+    return <Redirect to="/" />;
   }
 
   return (
