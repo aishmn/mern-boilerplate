1d67ae557feeda32a387a6afa7b6017361940720
aishmn <manish.sinuwari@gmail.com>
2020-05-28 21:20:35 +0545

    fixed signup bug

--- models/userModel.js
+++ models/userModel.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ["user", "guide", "lead-guide", "admin"],
+    enum: ["user", "admin"],
     default: "user",
   },
   password: {
